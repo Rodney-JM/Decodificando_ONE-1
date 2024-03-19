a5e4e3c9129e079e6506bd36d8aaaed1c1e491cc
Rodney <rondineypatricio@gmail.com>
2024-03-19 14:58:51 -0300

    Project complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,14 @@ const outputDesc = document.querySelector("#desc");
 // Buttons
 const scriptBtn = document.querySelector("#cpt_btn");
 const desScriptBtn = document.querySelector("#dcpt_btn");
+const copyBtn = document.querySelector("#copy_btn");
+const cleanDisplay = document.querySelector("#clean_btn");
 
 const displayOutput = document.querySelector("#displayTexts");
 
 const inputText = document.querySelector("#ipt_area");
+
+let copyText = '';
 
 scriptBtn.addEventListener('click', () => {
     womanImg.remove();
@@ -44,6 +48,7 @@ scriptBtn.addEventListener('click', () => {
                 break;
         }
     }
+    copyText = newText;
     let newOutput = document.createElement('p');
     newOutput.textContent = newText;
 
@@ -54,38 +59,46 @@ desScriptBtn.addEventListener('click', () => {
     womanImg.remove();
     outputDesc.remove();
     outputTitle.remove();
+    
 
     let textChang = inputText.value;
 
-    // Dividir texto em um array de caracteres
-    let textArray = textChang.split("");
-
     let newText = '';
 
-    for (let i = 0; i < textArray.length; i++) {
-        switch (textArray[i]) {
-            case 'enter':
-                newText += 'e';
-                break;
-            case 'imes':
-                newText += 'i';
-                break;
-            case 'ai':
-                newText += 'a';
-                break;
-            case 'ober':
-                newText += 'o';
-                break;
-            case 'ufat':
-                newText += 'u';
-                break;
-            default:
-                newText += textArray[i];
-                break;
-        }
-    }
+    textChang = textChang.replaceAll('ufat', 'u');
+    textChang = textChang.replaceAll('enter', 'e');
+    textChang = textChang.replaceAll('imes', 'i');
+    textChang = textChang.replaceAll('ai', 'a');
+    textChang = textChang.replaceAll('ober', 'o');
+
+    newText = textChang;
+    copyText = newText;
+    
     let newOutput = document.createElement('p');
     newOutput.textContent = newText;
 
     displayOutput.appendChild(newOutput);
 });
+
+copyBtn.addEventListener('click', ()=>{
+    const textoParaCopiar = copyText;
+
+    if (!navigator.clipboard) {
+        console.error('A API navigator.clipboard não é suportada neste navegador.');
+    } else {
+        navigator.clipboard.writeText(textoParaCopiar)
+            .then(() => {
+                console.log('Texto copiado com sucesso para a área de transferência!');
+            })
+            .catch(err => {
+                console.error('Erro ao copiar o texto para a área de transferência:', err);
+            });
+    }
+
+})
+
+cleanDisplay.addEventListener('click', ()=>{
+    while(displayOutput.children.length > 0){
+        displayOutput.removeChild(displayOutput.firstChild);
+    }
+})
